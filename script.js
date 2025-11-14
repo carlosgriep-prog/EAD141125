57e1de027ad9702a2f7881d39e03d9ec79ad1b0e
carlosgriep-prog <carlos.griep@aluno.senai.br>
2025-11-14 11:08:43 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Seleciona o botão pelo id
+const botao1 = document.getElementById("botao1");
+const botao2 = document.getElementById("botao2");
+
+// Botão academias
+botao1.addEventListener("click", function() {
+    window.location.href = "https://www.google.com/search?q=academias+de+artes+marciais+em+caxias+do+sul";
+});
+
+// Botão melhore suas habilidades
+botao2.addEventListener("click", function() {
+    window.location.href = "https://www.bauerfeind.com.br/como-melhorar-a-performance-nas-artes-marciais?srsltid=AfmBOop-pO3NQqzpBh5fprrQmqUQS6VseWBHUVxkGFEl3lHCAjQ92zKN";
+});
+
+const lutador = document.getElementById("lutador");
+
+// imagem atual e imagem nova
+const imagemOriginal = "Bruce_Irvin_TTT2-400x361.webp";
+const imagemNova = "Sagat.webp";
+
+let trocado = false;
+
+lutador.addEventListener("click", function () {
+    if (!trocado) {
+        lutador.src = imagemNova; 
+        trocado = true;
+    } else {
+        lutador.src = imagemOriginal;
+        trocado = false;
+    }
+});
